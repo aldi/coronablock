a1dacc1f92e3440d5aed06dca2ebd4fc441d5b7d
aldi <15065804+aldi@users.noreply.github.com>
2020-04-09 16:20:47 +0300

    Published v1.0

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,30 @@
 $(document).ready(function () {
     $('a#donation').click(donation);
     $('a#share').click(share);
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "getCount" }, function (count) {
-            document.querySelector(".blocked").textContent = count;
-        });
-    });
+    $('a#report').click(report);
 });
 function share() {
-    chrome.tabs.create({ url: "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fremove-all-politics-from%2Fgbbfjddgaoffjkpjgbpnfkmmepgpmdcl%3Fhl%3Den&amp;src=sdkpreparse" });
+    chrome.tabs.create({ url: "https://www.facebook.com/sharer/sharer.php?u=" });
 }
 function donation() {
     chrome.tabs.create({ url: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7TYGLYKXSR9NQ&source=url" });
 }
+
+function report() {
+    chrome.tabs.create({ url: "https://forms.gle/KS9EeYtchGMhRxP67" });
+}
+
+//ΑΥΤΟ ΤΟ ΠΑΠΑΡΙ ΕΔΩ ΘΑ ΜΑΣ ΧΑΛΑΣΕΙ ΤΗ ΔΟΥΛΕΙΑ ΟΛΗ
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: "getCount" }, function (count) {
+        if (chrome.runtime.lastError) {
+            document.querySelector(".warning").style.display = "block";
+            document.querySelector("body").style.opacity = "0.5";
+        }
+        if (count != undefined) {
+            document.querySelector(".blocked").textContent = count;
+            document.querySelector(".warning").style.display = "none";
+        }
+    });
+    // }
+});
